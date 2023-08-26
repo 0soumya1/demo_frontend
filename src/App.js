3c4175642b38e3341871d3b5fc038a2daeca17fc
Soumya <soumyaguptabond@gamil.com>
2023-08-26 23:32:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const App = ()=>{
+  const[flowers, setFlowers]= useState("");
+
+  // const url="backend-smoky-eight.vercel.app";
+
+  const getFlowers = async ()=>{
+    
+        let result = await fetch("http://localhost:5000/flowers");
+        result = await result.json();
+       setFlowers(result);
+  }
+ console.log(flowers, "flowers")
+  return(
+     <div>
+      <h2>dummy</h2>
+      {
+     flowers.map((item)=>{
+      return(
+        <ul>
+          <li>{item.name}</li>
+        </ul>
+      )
+     })
+    } 
+      <button onClick={getFlowers}>Submit</button>
+     </div>
+  )
+}
+
+export default App;
