520a4c5f96b19e93c3b068f7b3b16a3947ce39c3
Soumya <soumyaguptabond@gamil.com>
2023-08-31 20:34:04 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { useState } from "react";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 const App = ()=>{
   const[flowers, setFlowers]= useState("");
 
-  const url="https://backend-smoky-eight.vercel.app/";
+  const BASE_URL="https://backend-smoky-eight.vercel.app/";
   // const url1 = "http://localhost:5000/"
 
-  const getFlowers = async ()=>{
+  const getFlowers = ()=>{
     
-        let result = await fetch(url+"flowers");
-        result = await result.json();
-       setFlowers(result);
-  }
+//         let result = await fetch(url+"flowers");
+//         result = await result.json();
+//        setFlowers(result);
+let url = BASE_URL + "flowers"
+axios.get(url).then((res)=>{
+  console.log(res,"response from api")
+     if (res.data) {
+        setFlowers(res.data)
+      } else {
+        toast.error("error in getting Items");
+      }
+}).catch((err)=>{
+  console.log(err,"error in api call")
+})
+ }
  console.log(flowers, "flowers")
+
+ 
   return(
      <div>
       <h2>dummy</h2>
@@ -27,7 +42,7 @@ const App = ()=>{
       )
      })
     } 
-     
+     <Toaster/>
      </div>
   )
 }
